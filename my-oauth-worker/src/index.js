686c9ea979425f03df8229ddfdd3778ab9f8fb03
Oswaldo Fabrizio De Los Santos Ascencio <ofdelossantosa@gmail.com>
2026-09-18 23:03:36 -0400

    feat(worker): initialize cloudflare worker to retrieve access/refresh token from Google oauth2 endpoint

---
+++ my-oauth-worker/src/index.js
@@ -0,0 +1,61 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+export default {
+  async fetch(request, env) {
+    if (request.method === "OPTIONS") {
+      return new Response(null, {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "POST, OPTIONS",
+          "Access-Control-Allow-Headers": "Content-Type",
+        },
+      });
+    }
+
+    try {
+      const body = await request.json();
+      const params = new URLSearchParams({
+        client_id: env.CLIENT_ID,
+        client_secret: env.CLIENT_SECRET,
+      });
+
+      if (body.grant_type === "refresh_token") {
+        params.append("grant_type", "refresh_token");
+        params.append("refresh_token", body.refresh_token);
+      } else {
+
+		params.append("grant_type", "authorization_code");
+        params.append("code", body.code);
+        params.append("redirect_uri", body.redirect_uri);
+      }
+
+      const tokenResponse = await fetch("https://oauth2.googleapis.com/token", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: params,
+      });
+
+      const data = await tokenResponse.json();
+
+      return new Response(JSON.stringify(data), {
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+      });
+    } catch (err) {
+      return new Response(JSON.stringify({ error: err.message }), {
+        status: 500,
+        headers: { "Access-Control-Allow-Origin": "*" },
+      });
+    }
+  },
+};
